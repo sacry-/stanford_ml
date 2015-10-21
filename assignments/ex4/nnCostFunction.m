f7f0207c66ff3709845c8c0ca1700bafe652b609
sacry- <nitsche25@gmail.com>
2015-10-21 21:54:50 +0200

    finished ex5, removed unused code, started ex6

--- assignments/ex4/nnCostFunction.m
+++ assignments/ex4/nnCostFunction.m
@@ -101,11 +101,3 @@ end
 function v = dsum(seq)
     v = sum(sum(seq));
 end
-
-function v = h0(x, Theta1, Theta2)
-    a1 = x;
-    z2 = a1*Theta1';
-    a2 = [ones(size(z2,1),1) sigmoid(z2)];
-    z3 = a2*Theta2';
-    v = sigmoid(z3);
-end
