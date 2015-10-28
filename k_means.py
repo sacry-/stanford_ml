5cf73481c78ed00725ffe17fd2ceedcb594d3626
sacry- <nitsche25@gmail.com>
2015-10-29 00:36:49 +0100

    refactored plotting, 3d plot possible

--- k_means.py
+++ k_means.py
@@ -1,9 +1,11 @@
 from __future__ import division
 
 import numpy as np
+import sklearn.decomposition as deco
 import random
 import sys
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import colors
 import math
 import time
@@ -11,33 +13,22 @@ import six
 
 
 # Helper
-def create_sample(k, docs=400):
-  nums = [x for x in range(1,4)]
-  docs_per = docs / (k * 3)
-  accu = np.zeros((1,2))
-
-  def create_k_docs(accu):
-    for i in range(0, k):
-      variance = min(1.0 - random.random(), 0.5) + 0.5
-      num = random.choice( nums )
-      a = np.random.normal(num, variance, (docs_per, 2))
-      accu = np.concatenate((accu, a))
-    return accu
-
-  accu = create_k_docs(accu)
-  accu = create_k_docs(accu)
-
-  for i in range(0, k*10):
-    num = random.choice( nums )
-    a = np.random.normal(4, 30, (5, 2))
-    accu = np.concatenate((accu, a))
-
-  return np.array(accu, dtype=np.float32)
+def create_sample(num_docs, features):
+  splitted = num_docs / 5
+  a = 5 * np.random.random_sample((splitted, features))
+  b = -5 * np.random.random_sample((splitted, features))
+  c = np.random.normal(5, 4, (splitted, features))
+  d = np.random.normal(-5, 4, (splitted, features))
+  e = np.random.normal(0, 4, (splitted, features))
+  return np.concatenate((a,b,c,d,e))
 
 
 # Algorithm
 def distance(x, centroids):
   return np.linalg.norm(x - centroids, axis=1)**2
+
+def cost_func(x, centroids, c):
+  return (1 / x.shape[0]) * np.sum( distance(x, centroids[c.flatten()]) )
 
 def initial_centroids(x, k):
   space = list(range(0, x.shape[0]))
@@ -82,14 +73,11 @@ def kmeans(x, k, num_iter=40):
 
   return centroids, c, j_history
 
-def cost_func(x, centroids, c):
-  return (1 / x.shape[0]) * np.sum( distance(x, centroids[c.flatten()]) )
 
-# optima..
 def find_optimum(x, k=2, k_iter=10, num_iter=5):
   m = x.shape[0]
   centroids, c, j_history = None, None, None
-  cost = np.sum(x)**2
+  cost = float("inf")
   k_best, in_iter = k, 1
 
   iter_round = 0
@@ -99,8 +87,8 @@ def find_optimum(x, k=2, k_iter=10, num_iter=5):
       centroids_, c_, j_history_ = kmeans(x, k_next)
       new_J = cost_func(x, centroids_, c_)
 
-      print( "{}. cost: {}, k: {}".format(iter_round + k_off, new_J, k_next) )
       if new_J < cost:
+        print( "{}. new cost: {}, k: {}".format(iter_round + k_off, new_J, k_next) )
         in_iter = (i * k_iter) + (k_off + 1)
         cost, k_best = new_J, k_next
         centroids, c, j_history = centroids_, c_, j_history_
@@ -110,34 +98,70 @@ def find_optimum(x, k=2, k_iter=10, num_iter=5):
   return in_iter, cost, centroids, c
 
 
-def cluster_plot(x, centroids, c, k):
-  fig = plt.figure()
-  fig.suptitle("K means", fontsize=15)
+def reduce_dimensions(x, dims=3):
+  x = (x - np.mean(x, 0)) / np.std(x, 0)
+  pca = deco.PCA(dims)
+  y = pca.fit(x).transform(x)
+  print("Reducing columns of x {} by {} dims to {}".format(x.shape, dims, y.shape))
+  
+  return y
 
-  colors_ = set([x[0] for x in list(six.iteritems(colors.cnames))]) - set(["white", "black", "cyan", "magenta", "pink"])
-  point_colors = set(random.sample(colors_, k))
-  centroid_color = random.sample(colors_ - point_colors, 1).pop()
-  point_colors = list(point_colors)
+
+# Print out
+def get_colors(k):
+  ugly_colors = set(["white", "black", "cyan", "magenta", "pink"])
+  colors_ = set([x[0] for x in list(six.iteritems(colors.cnames))]) - ugly_colors
+  return list(set(random.sample(colors_, k)))
+
+def cluster_plot_3d(x, centroids, c, k):
+  fig = plt.figure()
+  fig.canvas.set_window_title("K means")
+  ax = fig.add_subplot(111, projection='3d')
+  fig.suptitle("k = {}".format(k), fontsize=12)
+  point_colors = get_colors(k)
 
   for i, cidx in enumerate(c):
-    plt.plot(x[i,0], x[i,1], point_colors[cidx], marker="o", zorder=-1)
+    ax.scatter(x[i,0], x[i,1], x[i,2], 
+      c=point_colors[cidx], marker='o', zorder=-1
+    )
   for i in range(0, k):
-    plt.scatter(centroids[i,0], centroids[i,1], s=150.0, c=centroid_color, marker='o', zorder=100)
+    ax.scatter(centroids[i,0], centroids[i,1], centroids[i,2], 
+      s=180.0, c=point_colors[i], marker='o', lw=2, zorder=100
+    )
   plt.show()
+
+def cluster_plot_2d(x, centroids, c, k):  
+  fig = plt.figure()
+  fig.canvas.set_window_title("K means")
+  fig.suptitle("k = {}".format(k), fontsize=12)
+  point_colors = get_colors(k)
+
+  for i, cidx in enumerate(c):
+    plt.scatter(x[i,0], x[i,1], 
+      c=point_colors[cidx], marker='o', zorder=-1
+    )
+  for i in range(0, k):
+    plt.scatter(centroids[i,0], centroids[i,1], 
+      s=180.0, c=point_colors[i], marker='o', lw=2, zorder=100
+    )
+  
+  plt.show()
+
 
 if __name__ == "__main__":
   if True:
-    x = create_sample(15)
-    in_iter, cost, centroids, c = find_optimum(x, k=1, k_iter=5, num_iter=3)
+    x = create_sample(800, 10)
+    x = reduce_dimensions(x, 2)
+    in_iter, cost, centroids, c = find_optimum(x, k=2, k_iter=20, num_iter=3)
     k_best = centroids.shape[0]
     print( "iteration: {}, cost: {}, k: {}".format( in_iter, cost, k_best ) )
-    cluster_plot(x, centroids, c, k_best)
+    cluster_plot_2d(x, centroids, c, k_best)
 
   else:
     k = 10
-    x = create_sample(15)
+    x = create_sample(500, 10)
+    x = reduce_dimensions(x, 2)
     centroids, c, j_history = kmeans(x, k)
-    cluster_plot(x, centroids, c, k)
-
+    cluster_plot_2d(x, centroids, c, k)
 
 
