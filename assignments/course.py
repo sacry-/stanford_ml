9aa659215711797737ea0dd3f2bb6a0605e3e532
sacry- <nitsche25@gmail.com>
2015-11-01 17:56:00 +0100

    data import

--- assignments/course.py
+++ assignments/course.py
@@ -3,19 +3,46 @@ from __future__ import division
 import numpy as np
 
 
-def peek(data, n=3):
-  print("\n".join(data.split("\n")[0:n]))
+class DataImport():
 
-def get_data(folder, file_, tokenizer=None, conversion=None):
-  if not tokenizer:
-    tokenizer = lambda data: [tuple(l.strip().split(",")) for l in data.split("\n") if l.strip()]
-  if not conversion:
-    conversion = lambda a: np.array(a, dtype=np.double)
-  base = "/Users/sacry/dev/projects/stanford_ml/assignments"
-  open_path = "{}/{}/{}".format(base, folder, file_)
-  with open(open_path, "r+") as f:
-    data = f.read()
-    return conversion(tokenizer(data))
+  def __init__(self, path_to):
+    self.path_to = path_to
+    self.raw_data = self.__read__(self.path_to)
+
+    self.__tokenizer = None
+    self.__converter = None
+
+  def transform(self):
+    tokens = self.__tokenize(self.raw_data)
+    transformation = self.__convert(tokens)
+    return transformation
+
+  def peek(self, n=3):
+    print( self.raw_data[0:n] )
+
+  def set_tokenizer(self, tokenizer):
+    self.__tokenizer = tokenizer
+
+  def set_converter(self, converter):
+    self.__converter = converter
+
+  def __tokenize(self, raw_data):
+    if not self.__tokenizer:
+      return [l.strip().split(",") for l in raw_data.split("\n") if l.strip()]
+    return self.__tokenizer(raw_data)
+
+  def __convert(self, tokens):
+    if not self.__converter:
+      return np.array(tokens, dtype=np.double)
+    return self.__converter(tokens)
+
+  def __read__(self, path_to):
+    base = "/Users/sacry/dev/projects/stanford_ml/assignments"
+    open_path = "{}/{}".format(base, path_to)
+    with open(open_path, "r+") as f:
+      return f.read()
+
+
 
 # Excercise 1
 def get_xyt(d):
@@ -31,7 +58,8 @@ def computeCost(x, y, theta):
   multiplier = 1 / (2 * len(y))
   return multiplier * sum(h0**2)
 
-d = get_data("ex1", "ex1data1.txt")
+importer = DataImport("ex1/ex1data1.txt")
+d = importer.transform()
 x, y, theta = get_xyt(d)
 print(computeCost(x, y, theta))
 
